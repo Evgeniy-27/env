acf8b7d2be9aafd5dbc59403b18d0fe7cd969352
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-09-03 22:53:27 +0700

    bcghfdktybz

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const { merge } = require('webpack-merge');
+const { merge } = require();
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
-const { merge } = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { merge } = require();
+const TerserPlugin = require();
+const OptimizeCSSAssetsPlugin = require();
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
